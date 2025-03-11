001eba28e054940585e9ffe0891892a6a3c1590c
NaKeRNarolino <narolinonaker@gmail.com>
2025-03-11 17:40:49 +0400

    Remove unnecessary features

--- src/client/kotlin/nakern/be_camera/BedrockCameraLibClient.kt
+++ src/client/kotlin/nakern/be_camera/BedrockCameraLibClient.kt
@@ -13,55 +13,55 @@ import org.lwjgl.glfw.GLFW
 object BedrockCameraLibClient : ClientModInitializer {
 	override fun onInitializeClient() {
 //		FadeDrawer.initialize();
-		val keybind = KeyBinding(
-			"becamera.init",
-			GLFW.GLFW_KEY_J,
-			"becamera"
-		)
-
-		ClientTickEvents.END_CLIENT_TICK.register {
-			if (keybind.wasPressed()) {
-				if (CameraManager.isActive()) {
-					CameraManager.clear();
-				} else {
-					CameraManager.setCamera(
-						CameraData(
-                            location = Vec3d(36.0, 71.0, -57.0),
-                            targetLocation = Vec3d(43.0, 63.0, -45.0),
-							easeOptions = EaseOptions(
-								easing = Easings::easeInOutBack,
-								duration = 2500
-							)
-                        )
-					)
-
-					ClientTimer(60) {
-						CameraManager.setFade(
-							CameraFadeOptions(
-                                fadeColor = Colors.WHITE,
-                                fadeInTime = 1000,
-                                holdTime = 1000,
-                                fadeOutTime = 1000
-                            )
-						)
-					}.start()
-
-					ClientTimer(100) {
-						CameraManager.setCamera(
-							CameraData(
-								location = Vec3d(24.0, 71.0, -70.0),
-								targetLocation = Vec3d(43.0, 63.0, -45.0),
-								easeOptions = EaseOptions(
-									easing = Easings::easeInOutBack,
-									duration = 2500
-								)
-							)
-						)
-					}.start()
-				}
-			}
+//		val keybind = KeyBinding(
+//			"becamera.init",
+//			GLFW.GLFW_KEY_J,
+//			"becamera"
+//		)
+//
+//		ClientTickEvents.END_CLIENT_TICK.register {
+//			if (keybind.wasPressed()) {
+//				if (CameraManager.isActive()) {
+//					CameraManager.clear();
+//				} else {
+//					CameraManager.setCamera(
+//						CameraData(
+//                            location = Vec3d(36.0, 71.0, -57.0),
+//                            targetLocation = Vec3d(43.0, 63.0, -45.0),
+//							easeOptions = EaseOptions(
+//								easing = Easings::easeInOutBack,
+//								duration = 2500
+//							)
+//                        )
+//					)
+//
+//					ClientTimer(60) {
+//						CameraManager.setFade(
+//							CameraFadeOptions(
+//                                fadeColor = Colors.WHITE,
+//                                fadeInTime = 1000,
+//                                holdTime = 1000,
+//                                fadeOutTime = 1000
+//                            )
+//						)
+//					}.start()
+//
+//					ClientTimer(100) {
+//						CameraManager.setCamera(
+//							CameraData(
+//								location = Vec3d(24.0, 71.0, -70.0),
+//								targetLocation = Vec3d(43.0, 63.0, -45.0),
+//								easeOptions = EaseOptions(
+//									easing = Easings::easeInOutBack,
+//									duration = 2500
+//								)
+//							)
+//						)
+//					}.start()
+//				}
+//			}
 
 //			println(Clock.System.now().toEpochMilliseconds())
-		}
+//		}
 	}
 }
